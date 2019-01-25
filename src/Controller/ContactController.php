420ae782a3c24d848f44fbeb60524670c1beb1a5
Melissa Viltet <melissaviltet@gmail.com>
2019-01-25 17:07:11 +0100

    mise en place de formualire de contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        $form =$this->createForm(ContactType::class);
+
+        return  $this->render('contact/index.html.twig', [
+            'contact_form' => $form->createView()
+        ]);
+
+
+    }
+}
