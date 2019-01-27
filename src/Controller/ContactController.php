dcb8388e79339c20ac365f78e30ca758f6d115b0
Melissa Viltet <melissaviltet@gmail.com>
2019-01-27 11:24:05 +0100

    swiftmaler

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -11,9 +12,33 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request, \Swift_Mailer $mailer)
     {
         $form =$this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $contactFormData = $form->getData();
+
+
+            $message = (new \Swift_Message('On a du courrier'))
+                ->setFrom($contactFormData['email'])
+                ->setTo('melissaviltet@hotmail.fr')
+                ->setBody(
+                    $contactFormData['message'],
+                    'text/plain'
+                )
+
+            ;
+            $mailer->send($message);
+
+            $this->addFlash(
+                'success',
+                'La missive est bien partie!'
+            );
+            return $this->redirectToRoute('contact');
+        }
 
         return  $this->render('contact/index.html.twig', [
             'contact_form' => $form->createView()
