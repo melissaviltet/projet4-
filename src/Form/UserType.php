a82ee37a4cbe7d145a692d8e1867cea11defe504
Melissa Viltet <melissaviltet@gmail.com>
2019-01-28 17:49:37 +0100

    table et relations

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,10 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Activity;
+use App\Entity\Gender;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,6 +18,17 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('firstname', TextType::class, ['label' => 'Prénom'])
+            ->add('gender', EntityType::class, [
+                'class' => Gender::class,
+                'choice_label' => 'name',
+                'label' => "Sexe"])
+            ->add('adress', TextType::class, ['label' => 'Adresse'])
+            ->add('activity', EntityType::class, [
+                'class'=> Activity::class,
+                'choice_label'=>'name',
+                'multiple'=>true])
             ->add('email')
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
